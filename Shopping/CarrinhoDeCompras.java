0ef3869dd52a8a320d5838981166028023f0f91f
Larissa Cavalcante <112430561+athensGit@users.noreply.github.com>
2023-11-08 13:13:26 -0300

    CarrinhoDeCompras.java

--- Shopping/CarrinhoDeCompras.java
+++ Shopping/CarrinhoDeCompras.java
@@ -1,5 +1,56 @@
 package Shopping;
 
-public class CarrinhoDeCompras {
+import java.util.ArrayList;
+import java.util.List;
 
+public class CarrinhoDeCompras {
+	private double valorTotal;
+	private int qtDoProduto;
+	private List<Produto> produtos;
+	
+	public CarrinhoDeCompras() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public int getQtDoProduto() {
+		return qtDoProduto;
+	}
+
+	public void setQtDoProduto(int qtDoProduto) {
+		this.qtDoProduto = qtDoProduto;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = new ArrayList<Produto>();
+	}
+	
+	public void addProduto(Produto produto) {
+		produtos.add(produto);
+	}
+	
+	public void removeProduto(Produto produto) {
+		produtos.remove(produto);
+	}
+	
+	public double calcularValor() {
+		valorTotal = 0.0;
+		double valorProduto;
+		for (Produto produto : produtos) {
+			valorProduto = produto.getValor() * qtDoProduto;
+			valorTotal += valorProduto;
+		}
+		return valorTotal;
+	}
 }
